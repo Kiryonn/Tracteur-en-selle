4093b33a4a2ef5fa8c4839929625a5666e5ee977
jojo1209 <78849419+jojo1209@users.noreply.github.com>
2022-12-15 16:18:34 +0100

    add dialogue velo

--- Assets/Scipts/GameManager.cs
+++ Assets/Scipts/GameManager.cs
@@ -5,43 +5,28 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
-    public FitnessEquipmentDisplay velo;
-    public bool connected;
-    public int instantaneousPower;
-    public float speed;
-    public float elapsedTime;
-    public int heartRate;
-    public int distanceTraveled;
-    public int cadence;
-
+    public DialogueVelo velo;
     public GameObject player;
+
+    public int pente;
 
     void Start()
     {
 
     }
 
+    private int penteA;
     // Update is called once per frame
     void Update()
     {
-        lectureVelo();
 
-
-        player.transform.position += speed * Time.deltaTime * player.transform.forward;
+        if(penteA != pente) { velo.pente(pente); penteA = pente; }
+        
+        player.transform.position += velo.instantaneousPower * Time.deltaTime * player.transform.forward;
 
     }
 
 
 
-    void lectureVelo() 
-    { 
-    
-        connected = velo.connected;
-        instantaneousPower = velo.instantaneousPower;
-        speed = velo.speed;
-        elapsedTime = velo.elapsedTime;
-        heartRate = velo.heartRate;
-        distanceTraveled = velo.distanceTraveled;
-        cadence = velo.cadence;
-    }
+
 }
