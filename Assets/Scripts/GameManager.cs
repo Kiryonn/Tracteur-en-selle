d61ee89e7c48754a096e09573def430af182ab0b
jojo1209 <78849419+jojo1209@users.noreply.github.com>
2022-12-15 16:35:54 +0100

    correction velo

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,15 +3,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
-	public FitnessEquipmentDisplay velo;
-	public bool connected;
-	public int instantaneousPower;
-	public float speed;
-	public float elapsedTime;
-	public int heartRate;
-	public int distanceTraveled;
-	public int cadence;
-
+	public DialogueVelo velo;
 	public GameObject player;
 
 	void Start()
@@ -22,22 +14,12 @@ public class GameManager : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		lectureVelo();
-		player.transform.position += speed * Time.deltaTime * player.transform.forward;
+		
+		player.transform.position += velo.speed * Time.deltaTime * player.transform.forward;
 
 	}
 
 
 
-	void lectureVelo() 
-	{ 
-	
-		connected = velo.connected;
-		instantaneousPower = velo.instantaneousPower;
-		speed = velo.speed;
-		elapsedTime = velo.elapsedTime;
-		heartRate = velo.heartRate;
-		distanceTraveled = velo.distanceTraveled;
-		cadence = velo.cadence;
-	}
+
 }
