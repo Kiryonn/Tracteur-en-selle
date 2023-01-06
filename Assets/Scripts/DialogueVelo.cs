8b826104667106e7d605826246254e09c98ab012
Kiryonn <60699390+Kiryonn@users.noreply.github.com>
2023-01-06 08:50:07 +0100

    {DEL} Unused / Redondent code

--- Assets/Scripts/DialogueVelo.cs
+++ Assets/Scripts/DialogueVelo.cs
@@ -5,63 +5,57 @@ using UnityEngine;
 public class DialogueVelo : MonoBehaviour
 {
 
-    public FitnessEquipmentDisplay velo;
-    public bool connected;
-    public float instantaneousPower;
-    public float speed;
-    public float elapsedTime;
-    public int heartRate;
-    public int distanceTraveled;
-    public int cadence;
+	public FitnessEquipmentDisplay velo;
+	public bool connected;
+	public float instantaneousPower;
+	public float speed;
+	public float elapsedTime;
+	public int heartRate;
+	public int distanceTraveled;
+	public int cadence;
+	public float maxSpeed = 24f;
 
-    public static DialogueVelo Instance;
+	public static DialogueVelo Instance;
 
-    public int veloPente=0;
-    void Start()
-    {
-        if (Instance != null)
-        {
-            Destroy(this);
-        }
-        else
-        {
-            Instance = this;
-        }
-    }
+	public int veloPente=0;
+	void Start() {
+		if (Instance != null)
+			Destroy(this);
+		else
+			Instance = this;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        lectureVelo();
-    }
+	// Update is called once per frame
+	void Update() {
+		lectureVelo();
+	}
 
 
-    void lectureVelo()
-    {
-        if (velo.connected)
-        {
-            connected = true;
-            instantaneousPower = velo.instantaneousPower;
-            speed = velo.speed;
-            elapsedTime = velo.elapsedTime;
-            heartRate = velo.heartRate;
-            distanceTraveled = velo.distanceTraveled;
-            cadence = velo.cadence;
-        }
-    }
+	void lectureVelo() {
+		if (velo.connected) {
+			connected = true;
+			instantaneousPower = velo.instantaneousPower;
+			speed = velo.speed;
+			elapsedTime = velo.elapsedTime;
+			heartRate = velo.heartRate;
+			distanceTraveled = velo.distanceTraveled;
+			cadence = velo.cadence;
+		}
+		if (speed > maxSpeed) {
+			speed = maxSpeed;
+		}
+	}
 
 
 
-    public void resitance(int n) 
-    {
-        velo.SetTrainerResistance(n);
-    }
+	public void resitance(int n) {
+		velo.SetTrainerResistance(n);
+	}
 
-    public void pente(int n)
-    {
-        if(n!= veloPente)
-            velo.SetTrainerSlope(n);
-        veloPente = n;
-    }
+	public void pente(int n) {
+		if(n!= veloPente)
+			velo.SetTrainerSlope(n);
+		veloPente = n;
+	}
 
 }
--- Assets/Scripts/Power.cs
+++ Assets/Scripts/Power.cs
@@ -1,114 +1,56 @@
-using System.Transactions;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Power : MonoBehaviour
 {
-    
-    public float TimeToDestroyObstacle;
 
-    //Parameters
-    private int _LocationNumber = 0;//if there is multiple spot
-    private float _timeToDestroyObstacle;
-    private Scrollbar JaugePower;
-    private ColorBlock colorPower;
+	public float TimeToDestroyObstacle;
 
-    //define global attributes
-    void Start()
-    {
-        JaugePower = GetComponent<Scrollbar>();
-        ResetObstacle();
-    }
+	//Parameters
+	private int _LocationNumber = 0;//if there is multiple spot
+	private float _timeToDestroyObstacle;
+	private Scrollbar JaugePower;
+	private ColorBlock colorPower;
 
-    void Update()
-    {
+	//define global attributes
+	void Start() {
+		JaugePower = GetComponent<Scrollbar>();
+		colorPower = JaugePower.colors;
+		ResetObstacle();
+	}
 
-        
-        if (DialogueVelo.Instance.instantaneousPower > 350f)
-        {
-            DialogueVelo.Instance.instantaneousPower = 350f;
-        }
-        JaugePower.size = DialogueVelo.Instance.instantaneousPower / 350f;
-        Debug.Log(JaugePower.size);
+	void Update() {
+		JaugePower.size = DialogueVelo.Instance.speed / 350f;
+		Debug.Log(JaugePower.size);
 
-            if (JaugePower.size > 0.795f)
-            {
-                colorPower = JaugePower.colors;
-                colorPower.normalColor = Color.red;
-                JaugePower.colors = colorPower;
-            }
+		if (JaugePower.size > 0.795f) {
+			// high speed
+			colorPower.normalColor = Color.red;
+			JaugePower.colors = colorPower;
+			GameManager.Instance.IncreaseBattery();
+		}
+		else if (JaugePower.size > 0.495f) {
+			// medium speed
+			colorPower.normalColor = Color.yellow;
+			JaugePower.colors = colorPower;
+			GameManager.Instance.IncreaseBattery();
+		} else {
+			// low speed
+			colorPower.normalColor = Color.green;
+			JaugePower.colors = colorPower;
+		}
+		JaugePower.size -= 0.04f * Time.deltaTime;
+	}
 
-            if (JaugePower.size > 0.495f && JaugePower.size < 0.795f)
-            {
-                colorPower = JaugePower.colors;
-                colorPower.normalColor = Color.yellow;
-                JaugePower.colors = colorPower;
-                GameManager.Instance.IncreaseBattery();
-            }
-            /*
-        if (Input.GetKey(KeyCode.Tab))
-        {
-
-            JaugePower.size += 0.005f;
-
-            if (JaugePower.size > 0.795f)
-            {
-                colorPower = JaugePower.colors;
-                colorPower.normalColor = Color.red;
-                JaugePower.colors = colorPower;
-            }
-
-            if (JaugePower.size > 0.495f && JaugePower.size < 0.795f)
-            {
-                colorPower = JaugePower.colors;
-                colorPower.normalColor = Color.yellow;
-                JaugePower.colors = colorPower;
-                GameManager.Instance.IncreaseBattery();
-            }
-        }*/
-
-        JaugePower.size -= 0.04f * Time.deltaTime;
-
-        if (JaugePower.size < 0.795f)
-        {
-            colorPower = JaugePower.colors;
-            colorPower.normalColor = Color.yellow;
-            JaugePower.colors = colorPower;
-        }
+	public Scrollbar GetJaugeObstacle() {
+		return JaugePower;
+	}
 
 
-        if (JaugePower.size < 0.495f)
-        {
-            colorPower = JaugePower.colors;
-            colorPower.normalColor = Color.green;
-            JaugePower.colors = colorPower;
-        }
-
-
-    }
-
-    public Scrollbar GetJaugeObstacle()
-    {
-        return JaugePower;
-    }
-
-    
-
-
-    //reset to starting value -> should disappear if not using Instantiate and/or using Destroy
-    public void ResetObstacle()//called by Tile[resetTile]
-    {
-
-        JaugePower.value = 0;
-        colorPower = JaugePower.colors;
-        colorPower.normalColor = Color.green;
-        JaugePower.colors = colorPower;
-    }
-
-    
-    
-
+	//reset to starting value -> should disappear if not using Instantiate and/or using Destroy
+	public void ResetObstacle() {
+		JaugePower.value = 0;
+		colorPower.normalColor = Color.green;
+		JaugePower.colors = colorPower;
+	}
 }
