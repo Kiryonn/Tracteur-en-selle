fefb9470ed63990c4812c13294b5e971b378a7de
Alexandre Metrix <tvv3show@gmail.com>
2023-06-29 09:35:10 +0200

    {Update} Some graphics 1

--- Assets/Graphic/Affutage_Affilage/ParticleEnabler.cs
+++ Assets/Graphic/Affutage_Affilage/ParticleEnabler.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class ParticleEnabler : StateMachineBehaviour
 {
     ParticleSystem particle;
-
+    AudioSource source;
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        source = animator.GetComponent<AudioSource>();
         particle = animator.gameObject.GetComponentInChildren<ParticleSystem>();
         base.OnStateEnter(animator, stateInfo, layerIndex);
+        source.Play();
         particle.Play();
     }
 
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         base.OnStateExit(animator, stateInfo, layerIndex);
+        source.Stop();
         particle.Stop();
     }
 }
